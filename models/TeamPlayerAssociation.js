a4fe987aa86c581599a6e9ea78f99a22b28c6257
Rafael Nicola <rafael.nicola@live.com>
2022-06-26 12:11:32 -0300

    Implementação do Paranoid

--- models/TeamPlayerAssociation.js
+++ models/TeamPlayerAssociation.js
@@ -2,7 +2,10 @@ const {DataTypes, Sequelize, Model} = require("sequelize");
 const {conn} = require("../db/connection");
 
 const TeamPlayerAssociation = conn.define("TeamPlayerAssociation", {
-
+    
+},{
+    paranoid: true,
+    deletedAt: 'destroyTime'
 });
 
 module.exports = TeamPlayerAssociation;
